18441abf935af1206e98c0fa96b19d5557b49dfa
Simon Westberg <simme.westberg@hotmail.com>
2018-12-10 17:01:55 +0100

    exercise building forms module 4

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -26,7 +26,7 @@ namespace Vidly.Controllers
         }
 
         // GET: Customers
-        public ActionResult Customers()
+        public ActionResult Index()
         {
 
             var customers = _context.Customers.Include(c => c.MembershipType).ToList();
@@ -36,9 +36,51 @@ namespace Vidly.Controllers
 
         public ActionResult New()
         {
+            var membershipTypes = _context.MembershipTypes.ToList();
 
+            var model = new CustomerFormViewModel
+            {
+                 MembershipTypes = membershipTypes
+            };
      
-            return View();
+            return View("CustomerForm", model);
+        }
+
+        [HttpPost]
+        public ActionResult Save(CustomerFormViewModel model)
+        {
+
+            if (model.Customer.Id == 0)
+                _context.Customers.Add(model.Customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == model.Customer.Id);
+                customerInDb.Name = model.Customer.Name;
+                customerInDb.BirthDate = model.Customer.BirthDate;
+                customerInDb.MembershipTypeId = model.Customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = model.Customer.IsSubscribedToNewsletter;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+        public ActionResult Edit(int id)
+        {
+
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var model = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", model);
         }
 
         [Route("Customers/Details/{id:regex(\\d{1}):range(1,12)}")]
