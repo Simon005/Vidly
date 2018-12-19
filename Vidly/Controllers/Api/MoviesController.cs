c98384046e7b42dda5aa9f9734d90a07440f965e
Simon Westberg <simme.westberg@hotmail.com>
2018-12-19 09:12:11 +0100

    making movies implementation

--- Vidly/Controllers/Api/MoviesController.cs
+++ Vidly/Controllers/Api/MoviesController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Data.Entity;
 using System.Net.Http;
 using System.Web.Http;
 using Vidly.Dtos;
@@ -25,9 +26,14 @@ namespace Vidly.Controllers.Api
 
         //GET /api/movies
 
-        public IEnumerable<MovieDto> GetMovies()
+        public IHttpActionResult GetMovies()
         {
-            return _myDbContext.Movies.ToList().Select(Mapper.Map<Movie, MovieDto>);
+            var moviesDto = _myDbContext.Movies
+                .Include(m => m.Genre)
+                .ToList()
+                .Select(Mapper.Map<Movie, MovieDto>);
+
+            return Ok(moviesDto);
         }
 
 
